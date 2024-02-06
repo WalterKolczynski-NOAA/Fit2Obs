d56d5b32f786b2d79e9dcff7e92628055b9f7609
David Huber <david.huber@noaa.gov>
2024-02-06 08:05:56 -0600

    Migrate to spack-stack; add Hercules support.
    This also removes references to Cheyenne which has been decomissioned.
    Refs #16 #15

--- modulefiles/fit2obs_s4.lua
+++ modulefiles/fit2obs_s4.lua
@@ -2,25 +2,13 @@ help([[
 Build environment for fit2obs on S4
 ]])
 
-prepend_path("MODULEPATH", "/data/prod/hpc-stack/modulefiles/stack")
+prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
 
-local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
-local license_ver=os.getenv("license_ver") or "S4"
-local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.4"
-local hpc_impi_ver=os.getenv("hpc_impi_ver") or "18.0.4"
+local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.0"
 
-local jasper_ver=os.getenv("jasper_ver") or "2.0.25"
-local zlib_ver=os.getenv("zlib_ver") or "1.2.11"
-local libpng_ver=os.getenv("libpng_ver") or "1.6.35"
-
-load(pathJoin("hpc", hpc_ver))
-load(pathJoin("hpc-intel", hpc_intel_ver))
-load(pathJoin("hpc-impi", hpc_impi_ver))
-load(pathJoin("license_intel", license_ver))
-
-load(pathJoin("jasper", jasper_ver))
-load(pathJoin("zlib", zlib_ver))
-load(pathJoin("libpng", libpng_ver))
+load(pathJoin("stack-intel", stack_intel_ver))
+load(pathJoin("stack-impi", stack_impi_ver))
 
 load("fit2obs_common")
 
