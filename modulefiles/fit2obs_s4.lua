34c91b27b7146f7532cd06a73d7b8b550ec88f4c
David Huber <david.huber@noaa.gov>
2024-02-07 12:52:51 +0000

    Fix S4's module file. #16

--- modulefiles/fit2obs_s4.lua
+++ modulefiles/fit2obs_s4.lua
@@ -2,13 +2,13 @@ help([[
 Build environment for fit2obs on S4
 ]])
 
-prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.6.0/envs/gsi-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
 local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
 local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.0"
 
 load(pathJoin("stack-intel", stack_intel_ver))
-load(pathJoin("stack-impi", stack_impi_ver))
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 load("fit2obs_common")
 
